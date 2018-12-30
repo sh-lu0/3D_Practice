30521add68445ee6b2c2d21d24ef25855564b4a8
sh-lu0 <sh-lu0@users.noreply.github.com>
2018-12-31 00:51:46 +0900

    インスタンス生成の練習

---
+++ Assets/DropTofu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTofu : MonoBehaviour {
+
+public GameObject MyTofu;
+private int TofuCount = 0;
+
+	void Start () {
+		InvokeRepeating("DropOne", 2f,1f);
+	}
+	void DropOne() {
+		TofuCount++;
+		Instantiate(MyTofu, new Vector3(0,5,0), Quaternion.identity);
+		if(TofuCount==100){
+			CancelInvoke();
+		}
+	}
+}
