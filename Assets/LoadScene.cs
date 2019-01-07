11fe7f59174187df2e82d6cd00c70f911b5d112c
sh-lu0 <sh-lu0@users.noreply.github.com>
2019-01-08 08:11:39 +0900

    コルーチンの練習

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*コルーチンを使ってロードする練習 */
+
+public class LoadScene : MonoBehaviour {
+
+	// 0:読み込み前 1:読み込み開始 2:ローディング完了
+	private int loadState;
+	AsyncOperation nextScene;
+
+	void Update(){
+		if(loadState==2 && Input.GetMouseButtonDown(0)){
+			nextScene.allowSceneActivation = true;
+		}
+	}
+
+	void OnMouseDown () {
+		if(loadState>0){
+			return;
+		}
+		StartCoroutine("LoadNextScene");
+	}
+
+	void LoadNextScene () {
+		loadState = 1;
+		nextScene = Application.LoadLevelAsync("SCENE2");
+		nextScene.allowSceneActivation = false;
+		while(nextScene.progress < 0.9f){
+			Debug.Log(nextScene.progress * 100 + "%");
+		}
+		loadState = 2;
+	}
+
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+	private Transform myTransform;
+
+	void Start () {
+		myTransform = GetComponent<Transform>();
+	}
+
+	void Update () {
+		myTransform.Rotate(0, Time.deltaTime*360/3, 0);
+	}
+
+	void OnMouseDown(){
+		Application.LoadLevel("SCENE1");
+	}
+
+}
